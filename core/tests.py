f1690704458d15459af62ccf2e242913cc3b28d3
PatrickFS18 <patrick.souza@aluno.riogrande.ifrs.edu.br>
2024-09-30 17:17:28 -0300

    Iniciando App Inscrição

--- core/tests.py
+++ core/tests.py
@@ -11,6 +11,5 @@ class HomeTest(TestCase):
 
         self.assertEqual(200, self.response.status_code)
 
-        def test_template(self):
-            self.assertTemplateUsed(self.response,'index.html')
-           
+    def test_template(self):
+        self.assertTemplateUsed(self.response,'index.html')
